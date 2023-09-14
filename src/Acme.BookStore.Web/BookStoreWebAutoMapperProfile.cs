94ca0a58f8177687b50a6366c8848164a49d7093
syedhsan <ahsan.afzal@whiteboxtech.net>
2023-09-14 18:00:27 +0500

    Add project files.

---
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore.Web;
+
+public class BookStoreWebAutoMapperProfile : Profile
+{
+    public BookStoreWebAutoMapperProfile()
+    {
+
+        CreateMap<BookDto, CreateUpdateBookDto>();
+        //Define your AutoMapper configuration here for the Web project.
+    }
+}
