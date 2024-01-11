ad313d16303bd8e784a11773907000c16248eeb1
asher.muneer <ahsan.afzal@whiteboxtech.net>
2024-01-11 18:49:31 +0500

    RedisServer Data InOut Completed

--- src/Acme.BookStore.Web/Pages/Books/EditModal.cshtml.cs
+++ src/Acme.BookStore.Web/Pages/Books/EditModal.cshtml.cs
@@ -35,10 +35,17 @@ namespace Acme.BookStore.Web.Pages.Books
         }
         public async Task OnGetAsync()
         {
-            //var bookDto = await _bookAppService.GetAsync(Id);
+            var IdsList = new List<Guid>();
+            var getBoks = await _bookAppService.GetListAsync(new PagedAndSortedResultRequestDto { SkipCount = 0, MaxResultCount = 20 });
+            foreach (var item in getBoks.Items)
+            {
+                IdsList.Add(item.Id);
+            }
             var bootDto = await GetAsync(Id);
             var book = ConvertToBookDto(bootDto);
             //var chache = await _bookCache.GetAsync(Id);
+            GetAllBooks(IdsList);
+
             Book = ObjectMapper.Map<BookDto, CreateUpdateBookDto>(book);
         }
         //public async Task AddAllCache(BookDto books)
@@ -83,6 +90,7 @@ namespace Acme.BookStore.Web.Pages.Books
                 throw;
             }
         }
+        
         private BookCacheItem ConvertToBookCacheItem(BookDto bookDto)
         {
             // Your conversion logic here
@@ -106,17 +114,61 @@ namespace Acme.BookStore.Web.Pages.Books
         }
         public async Task<IActionResult> OnPostAsync()
         {
-
-          
             await _bookAppService.UpdateAsync(Id, Book);
             return NoContent();
         }
+
+        #region GetorAddAllData
+        public async Task GetAllBooks(IEnumerable<Guid> bookIds)
+        {
+            //var getBoks = await _bookAppService.GetListAsync(new PagedAndSortedResultRequestDto { SkipCount = 0, MaxResultCount = 20 });
+            var IdsList = new List<Guid>();
+            var cacheKeys = bookIds.Select(id => id.ToString());
+            foreach (var item in bookIds)
+            {
+                IdsList.Add(item);
+            }
+            List<string> stringIdsList = IdsList.Select(id => id.ToString()).ToList();
+            var res = await _cache.GetOrAddManyAsync(
+                stringIdsList, //Cache key
+                async keys => await GetBooksFromDatabaseAsync(keys.Select(Guid.Parse)), // Assuming GetBooksFromDatabaseAsync takes IEnumerable<Guid>
+                () => new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5)
+                });
+        }
+        private async Task<List<KeyValuePair<string, BookCacheItem?>>> GetBooksFromDatabaseAsync(IEnumerable<Guid> bookIds)
+        {
+            var result = new List<KeyValuePair<string, BookCacheItem?>>();
+
+            foreach (var bookId in bookIds)
+            {
+                try
+                {
+                    var bookDto = await _bookAppService.GetAsync(bookId);
+                    var bookCacheItem = ConvertToBookCacheItem(bookDto);
+
+                    result.Add(new KeyValuePair<string, BookCacheItem?>(bookId.ToString(), bookCacheItem));
+                }
+                catch (Exception)
+                {
+                    // Handle the exception, you might log it or take other actions
+                    result.Add(new KeyValuePair<string, BookCacheItem?>(bookId.ToString(), null));
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+
         //private async Task<List<BookDto>> GetAllBooks()
         //{
         //    var requestDto = new PagedAndSortedResultRequestDto(); // You may need to customize this request object based on your needs
         //    var list = await _bookAppService.GetListAsync(requestDto);
         //    var sendList = new List<BookDto>();
-      
+
 
 
         //    return list;
